45dbe2fc5c8acd46ceb7be209298be94a85f7650
Troy Nunnally <troynunnally@brainrainsolutions.com>
2015-08-14 10:21:33 -0400

    remove android build

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# helloHarmanKardon
-# helloHarmanKardon
+Hello Harman Kardon Cordova
+=========================================
+
+A Hello World App for Harman Kardon Cordova plugin.
+
+
+## Installation
+
+This requires cordova 5.0+ ( current stable 1.0.0 )
+
+     cordova plugin add https://github.com/brainrainteam/harman-kardon-cordova
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.crudworks.cordova.plugins.cordovaboilerplateplugin/www/CordovaBoilerPlatePlugin.js",
+        "id": "org.crudworks.cordova.plugins.cordovaboilerplateplugin.CordovaBoilerPlatePlugin",
+        "clobbers": [
+            "CordovaBoilerPlatePlugin"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.crudworks.cordova.plugins.cordovaboilerplateplugin": "0.0.1"
 }
 // BOTTOM OF METADATA
 });
